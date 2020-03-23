5476726e17d1a64b4200c275b2641bf27e6b9037
Jake Urban <jake@stellar.org>
2020-03-23 14:37:37 -0700

    Require @time_bounds be set or set_timeout called

--- lib/stellar/transaction_builder.rb
+++ lib/stellar/transaction_builder.rb
@@ -1,33 +1,38 @@
 module Stellar
   class TransactionBuilder
 
-    attr_reader :source_account, :sequence_number, :base_fee, :timeout, :time_bounds, :memo, :operations
+    attr_reader :source_account, :sequence_number, :base_fee, :time_bounds, :time_bounds, :memo, :operations
 
     def initialize(
       source_account:, 
       sequence_number:, 
       base_fee: 100, 
-      timeout: 0, 
+      time_bounds: nil, 
       memo: nil
     )
       raise ArgumentError, "bad :source_account" unless source_account.is_a?(Stellar::KeyPair)
       raise ArgumentError, "bad :sequence_number" unless sequence_number.is_a?(Integer) && sequence_number > 0
-      raise ArgumentError, "bad :timeout" unless timeout.is_a?(Integer) && timeout >= 0
+      raise ArgumentError, "bad :time_bounds" unless time_bounds.is_a?(Stellar::TimeBounds) || time_bounds.nil?
       raise ArgumentError, "bad :base_fee" unless base_fee.is_a?(Integer) && base_fee >= 100
-
-      @timeout = timeout
-      now = Time.now.to_i
-      max_time = timeout != 0 ? now + timeout : timeout
 
       @source_account = source_account
       @sequence_number = sequence_number
       @base_fee = base_fee
-      @time_bounds = Stellar::TimeBounds.new(min_time: now, max_time: max_time)
+      @time_bounds = time_bounds
       @memo = self.make_memo(memo)
       @operations = Array.new
     end
 
     def build
+      if @time_bounds.nil?
+        raise "@time_bounds must be set during initialization or by calling set_timeout"
+      elsif !@time_bounds.min_time.is_a?(Integer) or !@time_bounds.man_time.is_a?(Integer)
+        raise "TimeBounds.min_time and max_time must be Integers"
+      elsif @time_bounds.max_time != 0 and @time_bounds.min_time > @time_bounds.max_time
+        raise "Timebounds.max_time must be greater than min_time"
+      elsif @time_bounds.max_time != 0 and @time_bounds.max_time < Time.now.to_i
+        raise "Timebounds.max_time must be in the future"
+      end
       @sequence_number += 1
       Stellar::Transaction.new(
         source_account: @source_account.account_id,
@@ -43,6 +48,23 @@ module Stellar
     def add_operation(operation)
       raise ArgumentError, "bad operation" unless operation.is_a? Stellar::Operation
       @operations.push(operation)
+      self
+    end
+
+    def set_timeout(timeout)
+      if timeout < 0
+        raise ArgumentError, "timeout cannot be negative"
+      end
+
+      timestamp = Time.now.to_i + timeout
+      if @time_bounds.nil?
+        @time_bounds = Stellar::TimeBounds.new(min_time: 0, max_time: timestamp)
+      elsif timeout == 0
+        @time_bounds.max_time = timeout
+      else
+        @time_bounds.max_time = timestamp
+      end
+
       self
     end
 
